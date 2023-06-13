b3851486b21fd2d210873672ceab93ae840311b0
SarahMuAli <136401981+SarahMuAli@users.noreply.github.com>
2023-06-13 05:38:57 +0300

    group aggregation

--- agg-b.js
+++ agg-b.js
@@ -5,13 +5,24 @@ require("dotenv").config();
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 
-const agg = [];
+const agg = [
+  {
+    $group: {
+      _id: "$customer.fullName",
+      "numberOfOrders": { $count: {} },
+      "totalItems": { $sum: { "$size": "$items" } },
+      "moneySpent": { $sum: "$total" }
+    }
+  },
+  { $sort: { "numberOfOrders": -1 } }
+];
 
 async function run() {
   try {
     const database = client.db("linkedin");
     const result = await database.collection("orders").aggregate(agg).toArray();
     console.log(result);
+    console.log(result.length);
   } catch (e) {
     console.log(e);
   } finally {
